a25c54b217cfda2b7e5ee61d883e0af9e3c491fb
tangent <tangent10crypto@gmail.com>
2021-06-12 14:32:22 -0400

    cleaning up, styling, adding buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
-import React, { useCallback, useEffect, useState } from 'react'
-// import MyWalletProvider from './contexts/MyWalletProvider'
+import React, {  useState } from 'react'
 
 declare const window: any;
 
-const App = () => {
-  const [accounts, setAccounts] = useState([]);
+interface account {
+  name: string
+  address: string
+}
 
-  const [name, setName] = '';
+const App = () => {
+
+
+  let startingAccounts = [] as account[];
+    const savedAccounts = localStorage.getItem("accounts");
+    if(savedAccounts && savedAccounts !== '') {
+      startingAccounts = JSON.parse(savedAccounts);
+    }
+  const [accounts, setAccounts] = useState(startingAccounts);
+
+  const [name, setName] = useState('');
   const [account, setAccount] = useState('');
   function onUnlockWallet() {
     try {
       if (window.ethereum) {
-        // need this? await window.ethereum.enable();
         window.ethereum
         .request({ method: 'eth_requestAccounts' })
         .then(handleAccountsChanged)
@@ -20,9 +30,6 @@ const App = () => {
           console.error(err)
         })
 
-        // register metamask events
-        // important: do this inside unlock wallet, otherwise you'll end up registering a ton of events
-        // there's probably a clean js way to ensure you only subscribe once but idk it right now
         window.ethereum.on('chainChanged', handleNetworkChange)
         window.ethereum.on('accountsChanged', handleAccountsChanged)
       }
@@ -39,52 +46,77 @@ const App = () => {
   }
   function handleAccountsChanged(accounts: string[]) {
     if (accounts.length === 0) {
-      // MAYBE: alert or something?
       console.log('Please connect to MetaMask.')
     } else if (accounts[0] !== account) {
       setAccount(accounts[0])
     }
   }
+
   return (
+    <div style={{margin: '0 auto', marginTop: '50px', width: '800px'}}>
+
     <div>
+      <span>current wallet: </span><span style={{marginLeft: '10px'}}>
+      {!!account ? (account) : (<button onClick={onUnlockWallet}>unlock wallet</button>) }
+      </span>
+      
+    </div>
+    <div>
+    <label htmlFor="name-input">name:</label>
+    <input id="name-input" onChange={(e) => setName(e.target.value)} />
+    </div>
 
-      <div>
-        hello. active: {account}
-      </div>
-      <div>
-      <label htmlFor="name-input">name:</label>
-      <input onChange={(e) => setName(e)} />
-      </div>
+    <hr />
 
-      <div>
-        --------------------------------------------
-      </div>
-
-      <div>
-        all accounts:
-      </div>
-
-      <table style={{border: '1px black solid'}}>
-      <thead>
-      <tr>
-      <th>Name</th>
-      <th>Address</th>
-      </tr>
-      </thead>
-      <tbody>
-      {accounts.map(a => <tr><td>{a.name}</td><td>{a.address}</td></tr>)}
-      </tbody>
-      </table>
-
-      <div>
-        <button onClick={onUnlockWallet}>Unlock Wallet</button>
+    <div>
+    all accounts:
       </div>
 
-      <div>
-        <button onClick={() => {
-          accounts.push({ address: account, name: name});
-        }}>Save address</button>
-      </div>
+
+<ol>
+    {accounts.map(a => <li style={{fontFamily: 'Consolas'}} key={a.name}>{a.address} : {a.name}</li>)}
+</ol>
+    {/* <table style={{border: '1px black solid'}}>
+    <thead>
+    <tr>
+    <th style={{border: '1px black solid', padding: '10px'}}>name</th>
+    <th style={{border: '1px black solid', padding: '10px'}}>address</th>
+    </tr>
+    </thead>
+    <tbody>
+    {accounts.map(a => <tr key={a.name}><td style={{border: '1px black solid', padding: '10px'}}>{a.name}</td><td style={{border: '1px black solid', padding: '10px'}}>{a.address}</td></tr>)}
+    </tbody>
+    </table> */}
+
+    <div>
+    <button onClick={() => {
+      // uh feels a bit hacky
+      // but apparently react doesn't want to redraw the screen
+      // unless setAccounts is called?
+      accounts.push({ address: account, name: name});
+      setAccounts([...accounts]);
+    }}>add address</button>
+    </div>
+
+    <div>
+    <button onClick={() => setAccounts([])}>clear accounts</button>
+    </div>
+
+    <div>
+    <button onClick={() => {
+      for(let i = 0; i < accounts.length; i++) {
+        console.log(`[${i}] : ${JSON.stringify(accounts[i])}`);
+      }
+    }}>list accounts </button>
+    </div>
+
+    <div>
+    <button onClick={() => {
+      // todo: save old accounts in like accounts.bak to allow for a redo
+      // otherwise this is pretty fragile
+      localStorage.setItem("accounts", JSON.stringify(accounts));
+    }}>save accounts</button>
+    </div>
 
     </div>
   )
